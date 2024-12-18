3be8cabb0ce6586ad32ee9c09a8f6879f23bf6d4
Amal Nekhdou <amallatifa1977@gmail.com>
2024-12-18 23:02:00 +0100

    version 10

--- env/app.py
+++ env/app.py
@@ -1,7 +1,8 @@
-from flask import Flask ,render_template, request, redirect, url_for, session
+from flask import Flask ,render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import os
+from datetime import datetime
 
 
 # Install pymysql as MySQLdb
@@ -9,6 +10,7 @@ pymysql.install_as_MySQLdb()
 
 # Create an instance of the Flask application
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'amalamal20'
 
 # Configuration of the database using environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql://amalnekh:amalsalma@localhost/gestion_Bibliothèque')
@@ -42,11 +44,12 @@ class Exemplaire(db.Model):
     IDDOC = db.Column(db.Integer, nullable=False)
     NUMERO_ORD = db.Column(db.Integer, nullable=False)
     DATE_ACHAT = db.Column(db.Date, nullable=False)
-    STATUT = db.Column(db.String(50), nullable=False)
-    ETAT = db.Column(db.String(50), nullable=False)
+    STATUT = db.Column(db.Enum('En rayon', 'En prêt', 'En retard', 'En réserve', 'En travaux'), nullable=False)
+    ETAT = db.Column(db.Enum('neuf', 'tres bon etat', 'bon etat', 'usage', 'endommage'), nullable=False)
 
+    
     def __repr__(self):
-        return f"<Exemplaire {self.ID_EXM}>"
+        return f"<Exemplaire {self.NUMERO_ORD}>"
 
 # Define routes
 @app.route('/')
@@ -103,6 +106,56 @@ def delete_document(document_id):
     db.session.commit()
     return redirect(url_for('list_documents'))
 
+@app.route('/exemplaires')
+def list_exemplaires():
+    exemplaires = Exemplaire.query.all()
+    return render_template('exemplaire.html', exemplaires=exemplaires)
+@app.route('/add-exemplaire')
+def add_exemplaire():
+    documents = Document.query.all()
+    return render_template('ajouterExm.html', documents=documents)
+   # return render_template('ajouterExm.html', action='Ajouter')
+
+@app.route('/save-exemplaire', methods=['POST'])
+def save_exemplaire():
+    iddocu = request.form['IDDOC']
+    Numero_ordre = request.form['NUMERO_ORD']
+    Date_achat = request.form['DATE_ACHAT']
+    statut = request.form['STATUT']  # Assurez-vous que les valeurs envoyées correspondent aux valeurs ENUM définies
+    etat = request.form['ETAT']
+    
+    new_exemplaire = Exemplaire(IDDOC=iddocu,NUMERO_ORD=Numero_ordre, DATE_ACHAT=Date_achat, STATUT=statut ,ETAT=etat)
+    
+    db.session.add(new_exemplaire)
+    db.session.commit()
+
+    # Redirection ou message de succès
+    flash('Exemplaire ajouté avec succès !')
+    return redirect(url_for('list_exemplaires'))
+
+@app.route('/edit-exemplaire/<int:exemplaire_id>', methods=['GET', 'POST'])
+def edit_exemplaire(exemplaire_id):
+    exemplaire = Exemplaire.query.get_or_404(exemplaire_id)
+
+    if request.method == 'POST':
+        exemplaire.NUMERO_ORD = request.form['numero']
+        exemplaire.DATE_ACHAT= request.form['date']
+        exemplaire.STATUT = request.form['statut']
+        exemplaire.ETAT= request.form['etat']
+
+        db.session.commit()
+        return redirect(url_for('list_exemplaires'))
+
+    return render_template('edit_exemplaire.html', exemplaire=exemplaire, action='Modifier')
+
+
+# Supprimer un document
+@app.route('/delete-exemplaire/<int:exemplaire_id>')
+def delete_exemplaire(exemplaire_id):
+    exemplaire_obj = Exemplaire.query.get_or_404(exemplaire_id)
+    db.session.delete( exemplaire_obj)
+    db.session.commit()
+    return redirect(url_for('list_exemplaires'))
 
 if __name__ == '__main__':
     app.run(debug=True)
